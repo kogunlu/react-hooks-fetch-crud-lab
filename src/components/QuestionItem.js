b4066e78f43bd313b5f7e7020fcc0a372c298a1f
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-06 13:31:46 +0300

    Completed Assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+  const [answerOfQuestion, setAnswerOfQuestion] = useState(correctIndex)
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +10,38 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+    })
+    .then(resp => resp.json())
+    .then(onDeleteQuestion(question))
+  }
+
+  function handleAnswerChange(e){
+    setAnswerOfQuestion(e.target.value)
+    
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        "correctIndex": e.target.value
+      })
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={e => handleAnswerChange(e)} value={answerOfQuestion}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList( { questions, onDeleteQuestion } ) {
+
+  const showQuestions = questions.map(item => <QuestionItem question={item} key={item.id} onDeleteQuestion={onDeleteQuestion} />)
+   
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{showQuestions}</ul>
     </section>
   );
 }
